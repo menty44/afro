52a31c8bdd7ff24430678c343e0727f7087e2b7e
Fredrick Oluoch <menty44@gmail.com>
2018-10-16 18:08:37 +0300

    first commit

---
+++ src/main/java/com/blaqueyard/kichap/KichapApplication.java
@@ -0,0 +1,12 @@
+package com.blaqueyard.kichap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KichapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KichapApplication.class, args);
+	}
+}
